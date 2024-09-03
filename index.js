3a0f3cfa90b33fb6cc87b1baa5c2e2d41f9958ab
suvigy <chouhansuvigya@gmail.com>
2024-09-03 17:59:56 +0530

    3Sept

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 const express = require("express");
 const urlRoute = require('./routes/url');
 const URL=require('./models/url')
+const staticRoute=require('./routes/staticRouter')
+const path = require("path");
 const { connectToMongodb } = require('./connect');
 const app = express();
 const PORT = 8001;
 
+app.set("view engine","ejs");
+app.set("views", path.resolve("./views"))
 // Middleware to parse JSON bodies (only needs to be included once)
 app.use(express.json());
 
 connectToMongodb('mongodb://127.0.0.1:27017/shorturl').then(() => console.log("MongoDB Connected"));
 
+app.get("/test", async (req,res)=>{
+    const allUrls=await URL.find({});
+   return res.render('home',{
+    urls:allUrls
+   })
+ 
+})
+
 app.use("/url", urlRoute);
-app.get("/:shortId", async(req,res)=>{
+app.use('/',staticRoute)  //ejs view
+
+app.get("/url/:shortId", async(req,res)=>{
     const shortId=req.params.shortId;
     const entry= await URL.findOneAndUpdate({
         shortId
     },
+    {$push:{
+        visitHistory:{timestamp:Date.now()}}
+    }
+    
+)
+res.redirect(entry?.redirectURL)
+})
 
-{$push:{
-    visitHistory:{timestamp:Date.now()}}
-})
-res.redirect(entry.redirectURL)
-})
 
 app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
 
